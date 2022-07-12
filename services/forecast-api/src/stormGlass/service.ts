b624a8bebaaafedc6b50703d4892df8f7f62ea08
marinaslucas <marinarlucas@gmail.com>
2022-07-11 22:48:03 -0300

    remove HttpServiceFactory, add axios

--- services/forecast-api/src/stormGlass/service.ts
+++ services/forecast-api/src/stormGlass/service.ts
@@ -1,25 +1,16 @@
 import Logger from '../../../../common/winston-logger';
-import { HttpServiceFactory } from '../../../../common/http-2';
-import { config } from '../config';
 import { StormGlassForecastResponse, StormGlassPoint, StormGlassNormalizedPoint } from '../types.ts/stormglassWeatherResponse';
-
-const HttpInstanceDefault = HttpServiceFactory().create({
-    baseURL: config.stormglass.url,
-    headers: {
-        Authorization: config.stormglass.auth
-    },
-    timeout: 10000
-});
+import Axios from 'axios';
 
 const stormGlassAPIParams = 'swellDirection%2CswellHeight%2CswellPeriod%2CwaveDirection%2CwaveHeight%2CwindDirection%2CwindSpeed';
 const stormGlassAPISource = 'noaa'
 
-export const StormGlassService = (httpRequest = HttpInstanceDefault) => {
+export const StormGlassService = (axios = Axios) => {
     const fetchPointsNormalized = async (lat: number, lng: number): Promise<StormGlassNormalizedPoint[]> => {
         try {
             Logger.info({ context: 'StormGlassService.fetchPointsNormalized START...', data: { lat: lat, lng: lng } })
             const path = `/v2/weather/point?params=${stormGlassAPIParams}&source=${stormGlassAPISource}&lat=${lat}&lng=${lng}`;
-            const response = await httpRequest.get<StormGlassForecastResponse>(path);
+            const response = await axios.get<StormGlassForecastResponse>(path);
             Logger.info({ context: 'StormGlassService.fetchPointsNormalized END.', data: response.data })
             return normalizedResponse(response.data);
         } catch (err) {
--- services/forecast-api/src/stormGlass/stormGlass.spec.ts
+++ services/forecast-api/src/stormGlass/stormGlass.spec.ts
@@ -1,23 +1,24 @@
 import StormGlassService from './service';
-import { HttpServiceFactory } from '../../../../common/http-2';
 import * as stormglass_weather_api_response from '../test/fixtures/stormglass_weather.json';
 import * as stormglass_weather_normalized from '../test/fixtures/stormglass_weather_normalized.json'
 
-const httpServiceFactory = HttpServiceFactory();
-const httpInstanceDefault = httpServiceFactory.create({
-  baseURL: 'www.teste.com.br',
-  headers: {
-    Authorization: 'authorization'
-  },
-  timeout: 10000
-});
+describe('StormGlassService', () => {
+  const get = jest.fn();
+  const axios = {
+    get
+  } as any;
 
-describe('StormGlass cliente', () => {
+  const stormGlassService = StormGlassService(axios);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
-    httpInstanceDefault.get = jest.fn().mockResolvedValue({ data: stormglass_weather_api_response })
-    const stormGlassService = StormGlassService(httpInstanceDefault);
+    get.mockResolvedValue({ data: stormglass_weather_api_response })
     const normalizedPoints = await stormGlassService.fetchPointsNormalized(lat, lng); //array de pontos normalizados
     expect(normalizedPoints).toEqual(stormglass_weather_normalized);
   })
